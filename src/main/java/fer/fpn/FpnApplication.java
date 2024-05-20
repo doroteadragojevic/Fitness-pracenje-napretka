11fd0d6d1d543a861575c81d3742e19593f369a5
Dorotea Dragojevic <dorotea.dragojevic@fer.hr>
2024-05-20 19:30:22 +0200

    Initial commit

---
+++ src/main/java/fer/fpn/FpnApplication.java
@@ -0,0 +1,13 @@
+package fer.fpn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpnApplication.class, args);
+	}
+
+}
